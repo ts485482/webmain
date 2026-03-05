260fc1fcf643b62df12cd4c60f8a5687f8fd79d7
leetaeshin <ts485482@gmail.com>
2026-03-05 10:07:16 +0900

    bb

--- qwe.py
+++ qwe.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, firestore
+import json
 
-cred = credentials.Certificate("Key.json")
+key_dict = json.loads(st.secrets["firebase_key"])
+cred = credentials.Certificate(key_dict)
 if not firebase_admin._apps:
     firebase_admin.initialize_app(cred)
 db = firestore.client()
